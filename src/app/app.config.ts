e0f66e5abc64091a1cfb24c560040a1f87b1595e
Roger Spring <roger.spring@gmail.com>
2025-09-19 11:58:58 -0700

    refactor: standardize SCSS import syntax and improve layout styles; enhance home component with progress indicator and updated card actions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { MAT_CARD_CONFIG } from '@angular/material/card';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    { provide: MAT_CARD_CONFIG, useValue: {
+        appearance: 'outlined',
+        elevation: '0',
+      }
+    }
   ]
 };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'glxg-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
