de8598521c1631552a921877eb11a39528437c94
Roger Spring <roger.spring@gmail.com>
2025-09-18 19:58:58 -0700

    Refactor SVG assets and update styles
    - Cleaned up SVG files by removing unnecessary metadata and attributes for better performance.
    - Adjusted dimensions in star.svg for consistency.
    - Added Material Icons stylesheet link in index.html for icon usage.
    - Implemented CSS custom properties for theming in _colors.scss.
    - Introduced a new font weight variable in _fonts.scss for better typography control.
    - Created custom Material theme in custom-material-theme.scss to enhance UI consistency.
    - Removed redundant CSS custom properties from main.scss to streamline styles.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'glxg-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
