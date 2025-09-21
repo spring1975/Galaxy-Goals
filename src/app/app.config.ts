282b0aac6f26c6aadb5932b0e34ecfdda8bd05ea
Roger Spring <roger.spring@gmail.com>
2025-09-20 20:37:06 -0700

    feat: add Angular PWA support; include service worker and manifest for offline capabilities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 import { MAT_CARD_CONFIG } from '@angular/material/card';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:3000'
+    }),
     { provide: MAT_CARD_CONFIG, useValue: {
         appearance: 'outlined',
         elevation: '0',
